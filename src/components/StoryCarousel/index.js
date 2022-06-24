4e90940ad12f28e47c81499ddd2dd52485423ffb
Miriamvdb <miriamvdb@live.nl>
2022-06-24 14:48:42 +0200

    Frontend/Feature2: Added stories in detailspage

--- src/components/StoryCarousel/index.js
+++ src/components/StoryCarousel/index.js
@@ -1,3 +1,15 @@
 export default function StoryCarousel(props) {
-  return <div></div>;
+  return (
+    <div>
+      {props.spaceWithStories.stories.map((story) => {
+        return (
+          <div>
+            <h1>{story.name}</h1>
+            <p>{story.content}</p>
+            <img alt={story.name} src={story.imageUrl} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/pages/SpaceDetailsPage/index.js
+++ src/pages/SpaceDetailsPage/index.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Space from "../../components/Space";
+import StoryCarousel from "../../components/StoryCarousel";
 import { fetchSpaceWithStories } from "../../store/space/actions";
 import { selectSpaceWithStories } from "../../store/space/selectors";
 
@@ -18,8 +19,8 @@ export default function SpacesDetailsPage() {
   if (!spaceWithStories) return <div>Loading..</div>;
 
   return (
-    <div className="container-spacedetails">
-      <div key={spaceWithStories.id}>
+    <div className="container-spacedetails-stories">
+      <div className="container-spacedetails" key={spaceWithStories.id}>
         <Space
           key={spaceWithStories.id}
           id={spaceWithStories.id}
@@ -28,6 +29,10 @@ export default function SpacesDetailsPage() {
           backgroundColor={spaceWithStories.backgroundColor}
           color={spaceWithStories.color}
         />
+      </div>
+
+      <div className="container-stories">
+        <StoryCarousel spaceWithStories={spaceWithStories} />
       </div>
     </div>
   );
