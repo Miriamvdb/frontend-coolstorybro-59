812a6ffcbc46e87dd0163c2ec36f8fcb3a738783
Miriamvdb <miriamvdb@live.nl>
2022-06-24 17:06:59 +0200

    Frontend/Feature3

--- src/components/Navigation/LoggedIn.js
+++ src/components/Navigation/LoggedIn.js
@@ -4,12 +4,15 @@ import { logOut } from "../../store/user/slice";
 import Button from "react-bootstrap/Button";
 import { selectUser } from "../../store/user/selectors";
 import Nav from "react-bootstrap/Nav";
+import NavbarItem from "./NavbarItem";
 
 export default function LoggedIn() {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
   return (
     <>
+      <NavbarItem path="/myspace" linkText="My Space" />
+
       <Nav.Item style={{ padding: ".5rem 1rem" }}>{user?.email}</Nav.Item>
       <Button onClick={() => dispatch(logOut())}>Logout</Button>
     </>
